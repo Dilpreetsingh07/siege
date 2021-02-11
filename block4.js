bebf8a313da614b5db825772f0e75b912724efbd
Dilpreetsingh07 <75797594+Dilpreetsingh07@users.noreply.github.com>
2021-02-11 08:17:49 +0530

    Add files via upload

---
+++ block4.js
@@ -0,0 +1,23 @@
+class Block4 {
+    constructor(x,y,width,height) {
+      var options = {
+        isStatic: false,
+        restitution:0.1,
+        density:0.5
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.x=x
+      this.y=y
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+        push();
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("grey");
+      rect(pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
